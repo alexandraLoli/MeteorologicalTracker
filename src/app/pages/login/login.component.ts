f0cb8bd5393e309816c1f7aa89bc8bd2da48db46
Deliu Cosmin Dragos <cosmin.deliu1993@gmail.com>
2024-12-11 21:58:35 +0200

    Integrare Firebase Authentication
    * Integrare Firebase Authentication
    
    * Google Login added
    
    ---------
    
    Co-authored-by: Cosmin Deliu <cosmin.deliu@itsmartsystems.eu>

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    imports: [FormsModule]
+})
+export class LoginComponent {
+    email: string;
+    password: string;
+  
+    constructor(private authService: AuthService) {
+      this.email = "";
+      this.password = "";
+    }
+  
+    signIn() {
+      this.authService.signIn(this.email, this.password)
+        .then(result => {
+          console.log('Login Successful', result);
+        })
+        .catch(error => {
+          console.error('Login Failed', error);
+        });
+    }
+
+    signInWithGoogle() {
+      this.authService.signInWithGoogle().then(user => {
+        if (user) {
+          console.log('User Logged In:', user);
+        } else {
+          console.log('Login failed or canceled.');
+        }
+      });
+    }
+
+  }
